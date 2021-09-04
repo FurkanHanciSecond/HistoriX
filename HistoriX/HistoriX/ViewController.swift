a2eff857355da95f876d202314c3a67bdd7180a7
Furkan Hanci <87238473+FurkanHanciSecond@users.noreply.github.com>
2021-09-04 21:21:00 +0300

    First Commit
    Previous App is exploading :D

---
+++ HistoriX/HistoriX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HistoriX
+//
+//  Created by Furkan Hanci on 9/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
