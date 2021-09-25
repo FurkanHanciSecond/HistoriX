9e9d2234f0c1ff8bc51d6574b437ab4a7350da14
Furkan Hanci <furkanhanci265@gmail.com>
2021-09-25 21:00:40 +0300

    HistoryDetail screen model completed.

--- HistoriX/HistoriX/CustomViews/Screens/HistoryDetailScreen/HistoryDetailModel.swift
+++ HistoriX/HistoriX/CustomViews/Screens/HistoryDetailScreen/HistoryDetailModel.swift
@@ -8,5 +8,25 @@
 import UIKit
 
 protocol HistoryDetailModelProtocol {
+    var data : MainModel? { get set }
+    func getData(content: @escaping () -> Void , errorContent: @escaping (HistoryError) -> Void)
+}
+
+class HistoryDetailModel : HistoryDetailModelProtocol {
+    var data: MainModel?
+    let service: HistoryServiceDelegate = HistoryService()
+
+    func getData(content: @escaping () -> Void, errorContent: @escaping (HistoryError) -> Void) {
+        service.HistoryRequest(router: MainModelRouter.getDatFromWelcome, decodingType: MainModel.self) { [ weak self] result in
+            switch result {
+              case .success(let data):
+                self?.data = data
+                content()
+              case .failure(let error):
+                errorContent(error)
+              }
+        }
+    }
+    
     
 }
