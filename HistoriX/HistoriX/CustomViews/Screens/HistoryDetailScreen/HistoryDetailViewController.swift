02271f066bf830ee87db84c3dae6cc20165db5b9
Furkan Hanci <furkanhanci265@gmail.com>
2021-09-28 22:15:14 +0300

    Sharing data between HistoryVC and HistoryDetailVC

--- HistoriX/HistoriX/CustomViews/Screens/HistoryDetailScreen/HistoryDetailViewController.swift
+++ HistoriX/HistoriX/CustomViews/Screens/HistoryDetailScreen/HistoryDetailViewController.swift
@@ -8,44 +8,58 @@
 import UIKit
 
 class HistoryDetailViewController: UIViewController {
-    private let detailLabel = HistoryLabel(fontSize: 17, fontWeight: .medium)
+    let label : UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+    let detailLabel : UILabel = UILabel(frame: CGRect(x: 10, y: 10, width: 200, height: 50))
     public var viewModel : HistoryDetailViewModel {
         didSet {
-            setup()
+            configure()
         }
     }
+    
     
     init(mainModel : MainModel) {
         self.viewModel = HistoryDetailViewModel(mainModel: mainModel)
         super.init(nibName: nil, bundle: nil)
+    }
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+            label.font = UIFont.preferredFont(forTextStyle: .footnote)
+        label.textColor = .white
+            label.center = CGPoint(x: 160, y: 284)
+            label.textAlignment = .center
+        detailLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+        detailLabel.textColor = .white
+        detailLabel.center = CGPoint(x: 160, y: 320)
+        detailLabel.textAlignment = .center
+
+        view.addSubviews(label , detailLabel)
+      configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setup()
-    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
-      super.viewWillAppear(animated)
+        super.viewWillAppear(animated)
         print(viewModel.datas)
     }
     
-    private func getUserInfo() {
-        viewModel.getData {
-        print("Datas parsed")
-       } errorContent: { error in
-         AlertManager.showAlert(message: error.rawValue, viewController: self)
-       }
-     }
+    private func setUpTexts(with mainModel : MainModel) {
+        self.label.text = mainModel.year
+        self.detailLabel.text = mainModel.text
+    }
     
-    private func setup() {
+    private func configure() {
+        setUpTexts(with: viewModel.datas!)
 
     }
- 
- 
+    
  
 }
